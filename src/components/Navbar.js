6796c2574628a463f452da9392fea574e2e9078e
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-07-16 22:00:11 -0700

    Adjusted tailwind css config and global default styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,21 +3,28 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo%21_Logo_2021.svg/1200px-ZDF_logo%21_Logo_2021.svg.png"
-                alt="company logo"
-                className="navbar__logo"
-            />
-            <div className="navbar__tabs">
-                <Link to="/about" className="navbar__tab">
-                    About
-                </Link>
-                <Link to="/login" className="navbar__tab">
-                    Login / Signup
-                </Link>
+        <nav className="flex justify-between bg-orange-300 text-white h-[3rem] pl-[1rem] align-middle">
+            <Link to="/">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo%21_Logo_2021.svg/1200px-ZDF_logo%21_Logo_2021.svg.png"
+                    alt="company logo"
+                    className="w-[6rem] h-[3rem]"
+                />
+            </Link>
+
+            <div className="flex align-middle">
+                <Tabs to="/about" name="About" />
+                <Tabs to="/login" name="Login/Signup" />
             </div>
         </nav>
+    );
+};
+
+const Tabs = (props) => {
+    return (
+        <Link to={props.to} className="flex items-center px-[1rem] h-auto hover:bg-yellow-200">
+            <p className='h-min'>{props.name}</p>
+        </Link>
     );
 };
 
