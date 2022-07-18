8b7f00c58f7080393274402fd412f1ded905c518
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-07-17 17:46:51 -0700

    initialized firebase cloud functions aka serverless backend?

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get('/', (request, response) => response.status(200).send('hello world'));
+
+// Listen on Firebase Server(
+exports.api = functions.https.onRequest(app);
