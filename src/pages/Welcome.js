13cc620a52690cd8045b7c670ac5d18a9fc40f8c
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-08-11 23:43:56 -0700

    welcome page finished

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Welcome = () => {
-    return <div className='welcome'>
-        Welcome Page
-    </div>;
+    return (
+        <div className="overflow-x-hidden flex justify-center">
+            <div className="w-[100%] fixed top-[20%] flex justify-center">
+                <Link className="text-2xl bg-orange-400 text-white px-5 py-2 rounded-2xl" to="/login">
+                    Start Scouting
+                </Link>
+            </div>
+
+            <img
+                src="/TakeScout_Logo.jpg"
+                alt="takeScout"
+                className=" min-w-[900px]"
+            />
+        </div>
+    );
 };
 
 export default Welcome;
