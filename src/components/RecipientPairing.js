08c4a0840bb81c0c9be95ff881511058c9c15b77
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2022-08-31 20:41:59 -0700

    Set up google maps api

--- src/components/RecipientPairing.js
+++ src/components/RecipientPairing.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Navigation from "./Navigation";
 
 const RecipientPairing = ({data}) => {
+  console.log(data);
+  const restaruantLatLong = {
+      lat: data.orders[0].restaurant_address.lat,
+      lng: data.orders[0].restaurant_address.lon,
+  };
+  const recipientLatLong = {};
+
   return (
       <div>
           <div>
               <div>Deliverer:{data.deliverer_name}</div>
               <div>Calculating Route</div>
+              <Navigation
+                  origin={restaruantLatLong}
+                  destination={restaruantLatLong}
+              />
           </div>
       </div>
   );
